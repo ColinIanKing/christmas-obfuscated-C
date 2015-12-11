8d7808f2ab95e46a98fce923d7cd1f3560012049
Colin Ian King <colin.king@canonical.com>
2015-12-11 23:35:36 +0000

    Add 2015 offering
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ 2015/tupper.c
@@ -0,0 +1,23 @@
+#include/**ptr++*/<gmp.h>
+#include /*#?*/ <stdio.h>
+int/* Christmas *//*\*\*/
+#  /* --2015--  */    de\
+fine const char         *
+#define     goto putchar(
+#define XII  ((XVII)-(7))
+#define Or(Not) mpz_##Not
+typedef _               ;
+const I=S;const _Float=f;
+_ main(){mpz_t NOT,OR;ch\
+ar*xor,*or,*nor;_ not,and
+,nand;Or(init)(OR);Or(in\
+it_set_str)(NOT,2+_Float,
+XII);Or(divexact_ui)(OR,\
+NOT,XVII);gmp_asprintf (&
+xor,"%Zx",&OR);for(not=0,
+or=xor;*or;){nand=(*or++)
+-97,nor=I+4*(nand<0?nand+
+'1':XII+nand);for(and=XII
+;and;and>>=1,not=(not+1)%
+XVII,goto*nor++))if(!not)
+goto XII)/*ColinKing*/;}}
