9bccb0f75edc07ec5b4d087d9e3a247a9afb9780
Colin Ian King <colin.king@canonical.com>
2016-12-12 21:31:23 +0000

    step 1, obfuscate variable names
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- 2016/snow.c
+++ 2016/snow.c
@@ -8,92 +8,71 @@
 #include <unistd.h>
 #include <locale.h>
 #include <signal.h>
+#include <string.h>
 
-#define BLACK_BLACK	0
-#define WHITE_BLACK	1
-#define BLACK_RED	2
-#define PILEFACTOR	15
+#define L	15
 
 typedef struct {
-	char t; /* type */
-	int x;
-	int y;
-	int stop;
-	int start;
-} flake_info_t;
+	short I[5];
+} F;
 
-typedef struct {
-	int x;
-	int y;
-} star_info_t;
-
-flake_info_t *fi;
-star_info_t *si;
-char flakes[] = ".',^\"$%*#";
-int  nflakes = 0;
+F *Q[2];
+char o[] = ".',^\"$%*#";
 struct winsize ws;
-int  *pile;
-int wx, wdx, wy;
-int resized;
+int *P;
+int I[7];
 
-#define FLSZ (sizeof(flakes) - 1)
-#define ST    8
-#define MAX(a,b) ((a > b) ? (a) : (b))
+#define l (sizeof(o) - 1)
+#define a    8
+#define u(a,b) (a > b ? a : b)
 
-static void get_ws(void)
-{
-	(void)ioctl(fileno(stdin), TIOCGWINSZ, &ws);
-}
-
-static int r(int max)
+int r(int x)
 {
 	srandom(getpid() * random());
-	return random() % max;
+	return random() % x;
 }
 
-static void wind(void)
+void E(void)
 {
-	wx = r(60);
-	wdx = r(2) == 1 ? -1 : 1;
-	wy = r(ws.ws_row);
+	I[0]=r(60);
+	I[5]=r(2)==1?-1:1;
+	4[I]=r(ws.ws_row);
 }
 
-static void sigwinch(int dummy)
+void h(int _)
 {
-	(void)dummy;
-	resized = 1;
+	3[I] = 1;
 }
 
-static void new_flakes(void)
+static void e(void)
 {
 	int i;
-	get_ws();
-	nflakes = (ws.ws_row * ws.ws_col) / 7;
-	free(pile);
-	pile = (int *)calloc(ws.ws_col, sizeof(int));
-	free(fi);
-	fi =  (flake_info_t*)calloc(nflakes, sizeof(flake_info_t));
-	for (i = 0; i < nflakes; i++) {
-		fi[i].t = r(FLSZ);
-		fi[i].x = r(ws.ws_col * FLSZ);
-		fi[i].y = r(ws.ws_row * FLSZ);
+	ioctl(0, TIOCGWINSZ, &ws);
+	I[1] = (ws.ws_row * ws.ws_col) / 7;
+	free(P);
+	P = (int *)calloc(ws.ws_col, sizeof(int));
+	free(1[Q]);
+	1[Q] =  (F*)calloc(I[1], sizeof(F));
+	for (i = 0; i < I[1]; i++) {
+		1[Q][i].I[0] = r(l);
+		1[Q][i].I[1] = r(ws.ws_col * l);
+		1[Q][i].I[2] = r(ws.ws_row * l);
 	}
-	free(si);
-	si = (star_info_t*)calloc(nflakes / ST, sizeof(star_info_t));
-	for (i = 0; i < nflakes / ST; i++) {
-		si[i].x = r(ws.ws_col);
-		si[i].y = r(ws.ws_row);
+	free(Q[0]);
+	Q[0] = (F*)calloc(I[1] / a, sizeof(F));
+	for (i = 0; i < I[1] / a; i++) {
+		Q[0][i].I[0] = r(ws.ws_col);
+		Q[0][i].I[1] = r(ws.ws_row);
 	}
-	pile[0] = PILEFACTOR;
+	P[0] = L;
 	for (i = 1; i < ws.ws_col; i++)
-		pile[i] = pile[i-1] + r(5) - 2;
-		if (pile[i] <= 0)
-			pile[i] = 1;
+		P[i] = P[i-1] + r(5) - 2;
+		if (P[i] <= 0)
+			P[i] = 1;
 }
 
 int main(void)
 {
-	int stop, frames;
         initscr();
         start_color();
         cbreak();
@@ -101,17 +80,16 @@ int main(void)
         nodelay(stdscr, 1);
         curs_set(0);
 
-	signal(SIGWINCH, sigwinch);
+	signal(SIGWINCH, h);
 	setlocale(LC_ALL, "");
-	init_pair(BLACK_BLACK, COLOR_BLACK, COLOR_BLACK);
-	init_pair(WHITE_BLACK, COLOR_WHITE, COLOR_BLACK);
-	init_pair(BLACK_RED, COLOR_RED, COLOR_BLACK);
-
-restart:
-	stop = frames = resized = 0;
-	new_flakes();
+	init_pair(0, COLOR_BLACK, COLOR_BLACK);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
+_:
+	memset(I,0,sizeof I);
+	e();
 	resizeterm(ws.ws_row, ws.ws_col);
-	wind();
+	E();
 
 	do {
 		int i,j,x,y;
@@ -122,52 +100,52 @@ restart:
 				mvaddch(y, x, ' ');
 
 		attron(COLOR_PAIR(1));
-		for (i=0;i<nflakes/ST;i++)
-			mvaddch(si[i].y,si[i].x, "+-"[r(2)]);
+		for (i=0;i<I[1]/a;i++)
+			mvaddch(Q[0][i].I[1],Q[0][i].I[0], "+-"[r(2)]);
 
-		for (j=0; j < FLSZ; j++) {
-			for (i=0;i<nflakes;i++) {
-				if ((fi[i].t != j) || (fi[i].stop))
+		for (j=0; j < l; j++) {
+			for (i=0;i<I[1];i++) {
+				if ((1[Q][i].I[0] != j) || (1[Q][i].I[3]))
 					continue;
-				if (fi[i].start)
-					mvaddch(fi[i].y / FLSZ,fi[i].x / FLSZ,flakes[fi[i].t]);
-				if (!fi[i].stop) {
-					fi[i].y += fi[i].t + 3;
-					if (fi[i].y / FLSZ >= (ws.ws_row) - (pile[fi[i].x / FLSZ] / PILEFACTOR / 2)) {
-						fi[i].y = 0;
-						if (fi[i].start)
-							pile[fi[i].x / FLSZ] += fi[i].t / 2;
-						fi[i].start = 1;
-						if (stop)
-							fi[i].stop = 1;
-						fi[i].x = r(ws.ws_col * FLSZ);
+				if (1[Q][i].I[4])
+					mvaddch(1[Q][i].I[2] / l,1[Q][i].I[1] / l,o[1[Q][i].I[0]]);
+				if (!1[Q][i].I[3]) {
+					1[Q][i].I[2] += 1[Q][i].I[0] + 3;
+					if (1[Q][i].I[2] / l >= (ws.ws_row) - (P[1[Q][i].I[1] / l] / L / 2)) {
+						1[Q][i].I[2] = 0;
+						if (1[Q][i].I[4])
+							P[1[Q][i].I[1] / l] += 1[Q][i].I[0] / 2;
+						1[Q][i].I[4] = 1;
+						if (6[I])
+							1[Q][i].I[3] = 1;
+						1[Q][i].I[1] = r(ws.ws_col * l);
 					}
-					if (wx < 20)
-						if (fi[i].y / FLSZ  > wy)
-							fi[i].x += (wx * wdx);
-					fi[i].x -= r(FLSZ) - (FLSZ / 2);
-					if (fi[i].x < 0)
-						fi[i].x = (ws.ws_col * FLSZ) - 1;
-					if (fi[i].x >= (ws.ws_col * FLSZ))
-						fi[i].x = 0;
+					if (I[0] < 20)
+						if (1[Q][i].I[2] / l  > 4[I])
+							1[Q][i].I[1] += (I[0] * I[5]);
+					1[Q][i].I[1] -= r(l) - (l / 2);
+					if (1[Q][i].I[1] < 0)
+						1[Q][i].I[1] = (ws.ws_col * l) - 1;
+					if (1[Q][i].I[1] >= (ws.ws_col * l))
+						1[Q][i].I[1] = 0;
 				}
 			}
 		}
-		wx--;
-		if (wx <= 0)
-			wind();
+		I[0]--;
+		if (I[0] <= 0)
+			E();
 		attron(COLOR_PAIR(0) | A_BOLD);
 		for (x=0;x<ws.ws_col;x++) {
 			int yy, yp;
-			if (x > 0 && pile[x] > pile[x-1] + 1) {
-				pile[x-1] += 1;
-				pile[x] -= 1;
+			if (x > 0 && P[x] > P[x-1] + 1) {
+				P[x-1] += 1;
+				P[x] -= 1;
 			}
-			if (x < ws.ws_col - 1 && pile[x] > pile[x+1] + 1) {
-				pile[x+1] += 1;
-				pile[x] -= 1;
+			if (x < ws.ws_col - 1 && P[x] > P[x+1] + 1) {
+				P[x+1] += 1;
+				P[x] -= 1;
 			}
-			yy = pile[x] / PILEFACTOR;
+			yy = P[x] / L;
 			yp = 2 * ws.ws_row - 1;
 			for (y=0; y < yy; y++, yp--) {
 				cchar_t w;
@@ -179,20 +157,20 @@ restart:
 				mvadd_wch(yp / 2, x, &w);
 			}
 		}
-		if (frames > 100) {
-			int y = (frames - 100)/3;
+		if (I[2] > 100) {
+			int y = (I[2] - 100)/3;
 			y = y <  ws.ws_row / 2 ? y : ws.ws_row / 2;
 			attron(COLOR_PAIR(2) | A_BOLD);
 			mvprintw(y, (ws.ws_col - 16)/2, "Happy Christmas!");
 		}
 		refresh();
-		if (resized)
-			goto restart;
+		if (3[I])
+			goto _;
 		usleep(90000);
-		frames++;
-		if (frames == 200)
-			stop = 1;
-	} while (frames < 500);
+		I[2]++;
+		if (I[2] == 200)
+			I[6] = 1;
+	} while (I[2] < 400);
 	clear();
 	endwin();
 	return 0;
