47e9c05ec61c8e24678448ca5d3847c8e72c972f
Colin Ian King <colin.king@canonical.com>
2020-12-13 12:21:45 +0000

    Add 2020 entry, requires aplay
    make clean
    make music
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ 2020/wavy.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <unistd.h>
+
+#define fa(x,d,J)j(x,d,o);o+=J;
+#define la(o,p)fa(o,p,0)
+#define re(o,p)fa(o,p,u>>2)
+#define mi(o,p)fa(o,p,u>>1)
+#define ti(o,p)fa(o,p,u)
+#define w 650<<10
+#define q(c,f)J(r=d+l,s=.0,a=.0,i=0;i++<e/c;a+=z,s+=v/(e/5))*r+++=s*f(a);J(;i\
+++<e/2;a+=z)*r+++=s*f(a);J(;i++<e;a+=z,s*=0.998)*r+++=s*f(a);
+int g b;g short _;g void V;const b u=4*9*25*49,p=u/2,n=u*2;g float y;y B[27];_
+d[w];y S(y x){G sin(x)/1.2;}y T(y x){G sin(x*2)/4+sin(x/4)/8+sin(x/8)/16;};V e
+(b o, b s){b i;J(i=o;i<w;i[d]+=*(i++-o+d)/s);}V j(b E, b e, b l){y f=B[E-65],t
+=(y)u/f,z=(10.0/1.59155)/t,s=.0,a,v=7000.;b i;_*r=d+l;q(7,S);q(9,T);};b main(V
+){b o,i;y f=220.0;char h[]={Y};J(o=write(1,h,'0'),i=0;i<27;f*=1.059463)*(i+++B
+)=f;o=0;mi('D',u)la(0x55,n)ti(0111,n)mi(77,u)ti('N',n)mi(0x4d,u)mi('K',u)mi(0\
+110,u)mi('O',u)ti(0x44,n)mi(78,u)mi('M',u)mi(89,u)mi(0x4f,u)ti('F',n)mi(77,u)\
+ti(0x4e,n)mi(68,u)la(0x55,n)la(80,n)o=0;la('('<<1,u)mi(0x50,u)la(0125,u)mi(0x\
+55,u)la('U',p)re(85,p)la(87,p)re(0127,p)la(80,p)re(0125,p)la(84,p)re('T',p)la(
+82,u)mi(78,u)la(0x4e,u)mi('N', u)la(78,u)mi('R',u)la('N', n)la(87,u)mi(0127,u)
+la(0x57,p)re('W',p)la(89,p)re('Y',p)la('R',u)la(87,p)re(0x57,p)la(0125,p)re(0\
+x55,p)la(0x50,u)mi(84,u)la(80,u)mi(0120,u)la('T',u)mi(0x50,u)la(84,u)la(89,u)\
+mi(0121,u)la('Y',p)re(77,p)la('Z',p)re(78,p)la(0124,u)la(89,p)re(0x59,p)re(87,
+p)la(82,u)la(0x55,u)mi('U',u)la('R',u)mi(0122,u)re(80,p)re(0x50,p)la(82,u)mi(\
+'R',u)mi(87,u)la(84,u)mi(80,u)la(0x55,n)la(0x50,n)ti('U',n)o+=6*p;J(i=1;i<10;\
+i++)e(i<<13,10*i);o=write(1,d,o+o);}
